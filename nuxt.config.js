9969230063c972ecba66e99def08a47229c068c5
Jesús Verduzco <jesus@verduzco.me>
2021-04-10 19:26:52 -0500

    change name

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - nuxt-ssr-versel-twtch',
-    title: 'nuxt-ssr-versel-twtch',
+    title: 'Nuxt SSR Vercel',
     htmlAttrs: {
       lang: 'en'
     },
